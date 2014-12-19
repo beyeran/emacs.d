2499068a4dd20fbdf04158ff0f8a1487ec1fb4a5
Andre P. Beyer <beyeran@gmail.com>
2014-12-19 17:26:15 +0100

    first test run with the new capture

--- init.el
+++ init.el
@@ -42,3 +42,17 @@
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files *dotfiles-dir* t "\\.org$"))
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/notes.org" "~/Dropbox/org/tasks.org" "~/Dropbox/org/calendar.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
